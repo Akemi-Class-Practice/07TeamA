3b04978e4eeedea9052d0427f66950744e1209db
rhoolan <rhoolan@gmail.com>
2023-09-04 15:46:30 +0900

    0904-Hoolan-Spring Bootの環境を制作

---
+++ TeamA/src/main/java/teamA/ex/TeamAApplication.java
@@ -0,0 +1,13 @@
+package teamA.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamAApplication.class, args);
+	}
+
+}
---
+++ TeamA/src/test/java/teamA/ex/TeamAApplicationTests.java
@@ -0,0 +1,13 @@
+package teamA.ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
