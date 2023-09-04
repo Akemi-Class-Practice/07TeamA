ec16c350459c659ae2fd2613eb9a7b2ada870d76
rhoolan <rhoolan@gmail.com>
2023-09-05 01:26:59 +0900

    0905-Hoolan-fix AdminService

--- TeamA/src/main/java/teamA/ex/service/AdminService.java
+++ TeamA/src/main/java/teamA/ex/service/AdminService.java
@@ -4,17 +4,23 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import blog.ex.model.dao.UserDao;
+import teamA.ex.model.dao.AdminDao;
 import teamA.ex.model.entity.AdminEntity;
 
 @Service
 public class AdminService {
 
+	@Autowired
+	private AdminDao adminDao;
+	
 	// Loginの処理
 	public AdminEntity login(String email, String password) {
 		// AdminDaoのfindByEmailを使ってユーザデータベースからユーザの情報を取得してAdminEntityでユーザーインスタンスを作る
-		AdminEntity adminEntity = adminDao.findByEmail(email);
+		AdminEntity adminEntity = adminDao.findByAdminEmail(email);
 		// 管理者インスタンスからユーザーのSALTを取得して
 		String salt = adminEntity.getSalt();
 		// ユーザーが入力したパスワードとSALTを組み合わせてhashPasswordに渡してハッシュする
