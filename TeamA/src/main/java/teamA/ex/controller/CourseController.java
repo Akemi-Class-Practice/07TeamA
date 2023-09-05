985376e9b77be0577f6e8a652a707a2e36431cb8
rhoolan <rhoolan@gmail.com>
2023-09-05 13:42:13 +0900

    0905-Hoolan-CourseController CourseService Edit

--- TeamA/src/main/java/teamA/ex/controller/CourseController.java
+++ TeamA/src/main/java/teamA/ex/controller/CourseController.java
@@ -1,5 +1,7 @@
 package teamA.ex.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.servlet.http.HttpSession;
+import teamA.ex.model.entity.AdminEntity;
+import teamA.ex.model.entity.CourseEntity;
+import teamA.ex.model.entity.UserEntity;
+import teamA.ex.service.CourseService;
 
 @RequestMapping("/home")
 
@@ -21,13 +27,11 @@ public class CourseController {
 	@Autowired
 	private HttpSession session;
 	
-	@GetMapping("/viewcourses")
-	
-	public String getCourseViewPage(Model model) {
+	@GetMapping("/adminviewcourses")
+	public String getAdminCourseViewPage(Model model) {
 		
 		// セッションから現在の管理者情報を取得するため、sessionオブジェクトを使用
 		AdminEntity adminList = (AdminEntity) session.getAttribute("admin");
-		Long adminId = adminList.getAdminId();
 		
 		// adminListから現在ログインしている人のユーザー名を取得
 		String adminName = adminList.getAdminName();
@@ -39,7 +43,25 @@ public class CourseController {
 		model.addAttribute("adminName", adminName);
 		model.addAttribute("courselist", courselist);
 		return "admin_view_courses.html";
+	}
+	
+	@GetMapping("/userviewcourses")
+	public String getUserCourseViewPage(Model model) {
 		
+		// セッションから現在の管理者情報を取得するため、sessionオブジェクトを使用
+		UserEntity user = (UserEntity) session.getAttribute("user");
+		Long userId = user.getStudentId();
+		
+		// adminListから現在ログインしている人のユーザー名を取得
+		String userName = user.getStudentName();
+		
+		//courseServiceのfindAllCoursePostメソッドを呼び出し、現在の管理者に関する講座を取得
+		//戻り値はCourseEntityのリストで、このリストをmodelに追加
+		 List<CourseEntity> courselist = courseService.findAll();
+		
+		model.addAttribute("userName", userName);
+		model.addAttribute("courselist", courselist);
+		return "user_view_courses.html";
 	}
 	
 	
--- TeamA/src/main/java/teamA/ex/service/CourseService.java
+++ TeamA/src/main/java/teamA/ex/service/CourseService.java
@@ -2,9 +2,9 @@ package teamA.ex.service;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import teamA.ex.model.dao.CourseDao;
 import teamA.ex.model.entity.CourseEntity;
 
@@ -16,5 +16,9 @@ public class CourseService {
 	public boolean createCourse(String courseName, int courseFee, String courseImage, LocalDate registerDate, LocalDate startDate, LocalDate finishDate, LocalTime lessonStartTime, int lessonDuration, Long adminId, int deleteFlag) {
 		courseDao.save(new CourseEntity(courseName, courseFee, courseImage, registerDate, startDate, finishDate, lessonStartTime, lessonDuration, adminId, deleteFlag));
 		return true;
+	}
+	
+	public List<CourseEntity> findAll(){
+		return courseDao.findAll();
 	}
 }
