724e54350299c5629225bd1b53f7d2fcf2830d20
rhoolan <rhoolan@gmail.com>
2023-09-07 14:08:29 +0900

    0907-Hoolan-User course view fix

--- TeamA/src/main/java/teamA/ex/controller/EditUserController.java
+++ TeamA/src/main/java/teamA/ex/controller/EditUserController.java
@@ -5,12 +5,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import jakarta.servlet.http.HttpSession;
 import teamA.ex.model.entity.UserEntity;
 import teamA.ex.service.UserService;
+
+@RequestMapping("/home")
 
 @Controller
 public class EditUserController {
