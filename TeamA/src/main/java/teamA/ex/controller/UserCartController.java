8d3479230bc7f7c518a115cb3793837819fd4e3c
acerola-se <acerola.se@gmail.com>
2023-09-08 17:40:54 +0900

    0908-fukuda-UserCartControllerのカート一覧機能作成

--- TeamA/src/main/java/teamA/ex/controller/UserCartController.java
+++ TeamA/src/main/java/teamA/ex/controller/UserCartController.java
@@ -10,10 +10,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.servlet.http.HttpSession;
-
 import teamA.ex.model.entity.CourseEntity;
 import teamA.ex.model.entity.UserEntity;
 import teamA.ex.service.CourseService;
@@ -21,91 +22,49 @@ import teamA.ex.service.CourseService;
 @Controller
 @RequestMapping("/home")
 public class UserCartController {
-	
+
 	// 講座(Course)にアクセスして操作するため、CourseServiceクラスを使えるようにする。
 	@Autowired
 	CourseService courseService;
-	
+
 	// HttpSessionを取得可能にしておく
 	@Autowired
 	HttpSession session;
-	
-	
-	// 購入履歴情報テーブル(transaction_history)にアクセスして操作するため、
-	// TransasctionHistoryServiceクラス　を使えるようにする
-//	@Autowired
-//	TransactionHistoryService transactionHistoryService;
-	
-	// 購入履歴情報テーブル(transaction_history)にアクセスして操作するため、
-	// TransactoinItemServiceクラス を使えるようにしておく
-//	@Autowired
-//	TransactionItemService transactionItemService;
-	
-	
-	// メール送信用クラス
-//	@Autowired
-//	MailSender mailSender;
-	
-	// ユーザーが購入した商品一覧テーブルにアクセスして操作するため、
-	// SubscriptionServiceクラスを使えるようにしておく
-//	@Autowired
-//	SubscriptionService subscriptionService;
-	
-	
-//	// メニュー画面 表示の処理
-//	@GetMapping("")
-//	public String getCourseViewPage(Model model) {
-//		
-//		// courseテーブルから公開されている講座情報のみ取得する
-//		List<CourseEntity> courseList = courseService.findCoursesForUser();
-//		
-//		// modelへ、courseテーブルから取得した情報をセットする。
-//		model.addAttribute("courseList", courseList);
-//		
-//		// modelへ、logincheckメソッドから取得した情報をセットする
-////		model.addAttribute("loginflg",logincheck());
-//		
-//		//modelへ、loginUserNameメソッドから取得した情報をセットする。
-////		model.addAttribute("userName",loginUserName());
-//		
-//		return"user_view_courses.html";
-//	
-//	
-//	}
-//	
-	
+
+
+
 	// ログアウトの処理
 	@GetMapping("/guest/view/courses")
 	public String getCourseLogoutViewPage(Model model) {
-		
+
 		// courseテーブルから公開されている講座情報のみ取得する
 		List<CourseEntity> courseList = courseService.findCoursesForUser();
-		
+
 		// modelへ、courseテーブルから取得した情報をセットする。
 		model.addAttribute("courseList", courseList);
-		
+
 		// modelへ、ログアウト処理のため、ログインフラグにfalseをセットする。
 //		model.addAttribute("loginflg",false);
-		
+
 		// modelへ、ログアウト処理のため、ログインしているユーザー情報にnullをセットする。
 //		model.addAttribute("userName", null);
-		
-		model.addAttribute("logoutMessage","ログアウトしました");
-		return"guest_view_courses";	
+
+		model.addAttribute("logoutMessage", "ログアウトしました");
+		return "guest_view_courses";
 	}
-	
+
 	// 講座詳細の表示処理
 	@GetMapping("/guest/view/courseinfo")
-	public String getCourseInfoViewPage(@PathVariable Long courseId,Model model) {
+	public String getCourseInfoViewPage(@PathVariable Long courseId, Model model) {
 		CourseEntity course = courseService.getCourse(courseId);
-		model.addAttribute("course",course);
+		model.addAttribute("course", course);
 //		model.addAttribute("loginFlg",logincheck());
 //		model.addAttribute("userName",loginUserName());
-		return"user_view_course_info.html";
+		return "user_view_course_info.html";
 	}
-	
-	public boolean isCourseExist(Long courseId,LinkedList<CourseEntity> list) {
-		Iterator<CourseEntity>ite = list.iterator();
+
+	public boolean isCourseExist(Long courseId, LinkedList<CourseEntity> list) {
+		Iterator<CourseEntity> ite = list.iterator();
 		boolean isExist = false;
 		while (ite.hasNext()) {
 			CourseEntity course = ite.next();
@@ -117,7 +76,7 @@ public class UserCartController {
 		return isExist;
 
 	}
-	
+
 //	public boolean loginCheck() {
 //		if (session.getAttribute("user") == null) {
 //			return false;
@@ -135,43 +94,53 @@ public class UserCartController {
 //			return null;
 //		}
 //	}
-		
-	
+
 	// カートへの追加機能
-	@GetMapping("add/to/cart/{courseId}")
+	@GetMapping("/add/to/cart/{courseId}")
 	public String addCourseToCart(@PathVariable Long courseId) {
 //		Long courseId = course.getCourseId();
 		LinkedList<CourseEntity> cartList = (LinkedList<CourseEntity>) session.getAttribute("cart");
 		CourseEntity course = courseService.getCourse(courseId);
 		cartList.add(course);
-		return"redirect:/home/view/user/courses";
+		return "redirect:/home/view/user/courses";
 	}
-		
-	
+
 	// カートからの削除機能
-	@GetMapping("delete/from/cart/{courseId}")
+	@GetMapping("/delete/from/cart/{courseId}")
 	public String deleteCourseFromCart(@PathVariable Long courseId) {
 //		Long courseId = course.getCourseId();
-		
+
 		LinkedList<CourseEntity> cartList = (LinkedList<CourseEntity>) session.getAttribute("cart");
-		
-		for(int idx = 0; idx < cartList.size(); idx++) {
-			if(cartList.get(idx).getCourseId().equals(courseId)) {
+
+		for (int idx = 0; idx < cartList.size(); idx++) {
+			if (cartList.get(idx).getCourseId().equals(courseId)) {
 				cartList.remove(idx);
 				break;
 			}
 		};
 		return "redirect:/home/user/view/cart";
 	}
-	
-	
-	
-	
-	
-	
+
+//	// カートの中身を見るための機能(もしかしたらいらない)
+//	@PostMapping("/home/user/view/cart")
+//	public String getCartPage(@RequestParam Long courseId, Model model) {
+//
+//		
+//			LinkedList<CourseEntity> list = (LinkedList<CourseEntity>) session.getAttribute("cart");
+//			return "user_view_cart.html";
+//		
+//
+//	}
+
+	// カートの中身を見るための機能
+	@GetMapping("/cart")
+	public String getCartPage(Model model) {
+		LinkedList<CourseEntity> list = (LinkedList<CourseEntity>) session.getAttribute("cart");
+		model.addAttribute("list", list);
+		return "user_view_cart.html";
+	}
 
 }
-
 
 
 
