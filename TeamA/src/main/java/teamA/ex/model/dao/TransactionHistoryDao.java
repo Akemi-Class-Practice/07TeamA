2536836606356c77b89cdb434874dfdfdd66509c
rhoolan <rhoolan@gmail.com>
2023-09-11 13:56:48 +0900

    0911-Hoolan-Fix

--- TeamA/src/main/java/teamA/ex/model/dao/TransactionHistoryDao.java
+++ TeamA/src/main/java/teamA/ex/model/dao/TransactionHistoryDao.java
@@ -6,9 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import jakarta.transaction.Transactional;
+import teamA.ex.model.entity.TransactionHistoryEntity;
 
 public interface TransactionHistoryDao extends JpaRepository<TransactionHistoryEntity, Long> {
 	TransactionHistoryEntity save(TransactionHistoryEntity transactionHistoryEntity);
+	
 	@Query(value="SELECT * FROM transaction_history WHERE student_id = ? ORDER BY transaction_id DESC LIMIT 1",
 			nativeQuery = true)
 	TransactionHistoryEntity getLatestByStudentId(Long studentId);
