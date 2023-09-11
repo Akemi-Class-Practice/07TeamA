2b460767f7c125b9f3e068cd4b84dcbec9538a4e
AN-SANGYEON <dou9876@gmail.com>
2023-09-11 15:51:46 +0900

    0911-an-main

--- TeamA/src/main/java/teamA/ex/model/dao/TransactionItemsDao.java
+++ TeamA/src/main/java/teamA/ex/model/dao/TransactionItemsDao.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import jakarta.transaction.Transactional;
+import teamA.ex.model.entity.TransactionItemsEntity;
 
 public interface TransactionItemsDao extends JpaRepository<TransactionItemsEntity, Long> {
 	TransactionItemsEntity save(TransactionItemsEntity transactionItemsEntity);
