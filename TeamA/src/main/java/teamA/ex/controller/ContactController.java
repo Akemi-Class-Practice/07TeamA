d323eb866d3fec44b26dbccbfd53174656a937fc
okashin123 <1594921760w@gmail.com>
2023-09-15 10:05:21 +0900

    cotanctcotrollerを修正しました

--- TeamA/src/main/java/teamA/ex/controller/ContactController.java
+++ TeamA/src/main/java/teamA/ex/controller/ContactController.java
@@ -1,18 +1,12 @@
 package teamA.ex.controller;
-
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import jakarta.servlet.http.HttpSession;
-import teamA.ex.model.entity.ContactEntity;
 import teamA.ex.model.entity.UserEntity;
 import teamA.ex.service.ContactService;
 
@@ -22,27 +16,31 @@ public class ContactController {
 
 	@Autowired
 	private ContactService contactService;
-	
+
 	@Autowired
 	HttpSession session;
+
 	// お問い合わせの画面の表示
 	@GetMapping("/user/contact/view")
-	public String getContactViewPage() {
-		return "user_contact.html";
+
+	public String getContactViewPage(Model model) {
+		UserEntity user = (UserEntity) session.getAttribute("user");
+		model.addAttribute("user", user);
+        return "user_contact.html";
 	}
-	//ユーザー側のお問い合わせ内容の保存
+
+	// ユーザー側のお問い合わせ内容の保存
 	@PostMapping("/user/contact/process")
-	public String ContactView( @RequestParam String contactTitle,
-		@RequestParam String contactDetail,  Model model) {
+	public String ContactView(@RequestParam String contactTitle, @RequestParam String contactDetail, Model model) {
 		UserEntity user = (UserEntity) session.getAttribute("user");
 		int isDone = 0;
 		Long studentId = user.getStudentId();
-		if (contactService.createContact( contactTitle, contactDetail, studentId, isDone)) {
-			return "user_view_courses.html";
-		}else {
-			return "user_contact.html";
+		if (contactService.createContact(contactTitle, contactDetail, studentId, isDone)) {
+			return "redirect:/user/contact/view";
+		} else {
+			return "redirect:/userlogin";
 		}
-		
+
 	}
-	
+
 }
